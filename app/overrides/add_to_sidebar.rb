c0a2d2640234f2ee240ece034590d2968ec361cf
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-22 19:49:29 +0200

    Creado hook

--- app/overrides/add_to_sidebar.rb
+++ app/overrides/add_to_sidebar.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => %q{spree/shared/_search},
+Deface::Override.new(:virtual_path => %q{spree/shared/_nav_bar},
                      :name => %q{add_to_sidebar},
                      :replace => %q{li#search-bar},
                      :text => %q{<li id="search-bar" data-hook>
