426bfb4ddcb25fd65a5981d11b1720f52009ee38
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-22 19:32:20 +0200

    Creado hook

--- app/overrides/add_to_sidebar.rb
+++ app/overrides/add_to_sidebar.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => %q{spree/shared/_search},
                      :name => %q{add_to_sidebar},
-                     :insert_after => %q{[#search-bar]},
+                     :insert_after => %q{<li id="search-bar" data-hook>},
                      :text => %q{<%= render 'shared/locales_dropdown' %>})
