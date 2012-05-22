d43d13030f5d9b82f7303e90bea295013d255b50
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-22 18:17:42 +0200

    Adaptacion a spree 1.1.0

---
+++ app/overrides/add_to_head.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => %q{shared/_head},
+                          :name => %q{add_to_head},
+                          :insert_after => %q{code[erb-loud]:contains('stylesheet_link_tag')},
+                          :text => %q{<%= stylesheet_link_tag 'dd' %><%= javascript_include_tag 'jquery.dd.js' %>})
--- lib/spree_locales_dropdown.rb
+++ lib/spree_locales_dropdown.rb
@@ -1,14 +1,15 @@
-require 'spree_core'
-require 'spree_locales_dropdown_hooks'
-
 module SpreeLocalesDropdown
   class Engine < Rails::Engine
-
+    railtie_name "spree_locales_dropdown"
+    
     config.autoload_paths += %W(#{config.root}/lib)
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env.production? ? require(c) : load(c)
+        Rails.application.config.cache_classes ? require(c) : load(c)
+      end
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
       end
     end
 
