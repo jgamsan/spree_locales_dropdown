6cd88e05acf44fa76ee8bacaa0b0297ef61674f7
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-22 19:10:52 +0200

    Creado hook

--- app/overrides/add_to_sidebar.rb
+++ app/overrides/add_to_sidebar.rb
@@ -1,4 +1,8 @@
 Deface::Override.new(:virtual_path => %q{spree/products/index},
                      :name => %q{add_to_sidebar},
-                     :insert_before => %q{[data-hook='homepage_sidebar_navigation']},
-                     :text => %q{<%= render 'shared/locales_dropdown' %>})
+                     :replace => %q{[data-hook='homepage_sidebar_navigation']},
+                     :text => %q{<%= render 'shared/locales_dropdown' %><% if "products" == @current_controller && @taxon %>
+      <%= render :partial => 'spree/shared/filters' %>
+    <% else %>
+      <%= render :partial => 'spree/shared/taxonomies' %>
+    <% end %>})
